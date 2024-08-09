f6f91ae9a95eda36e96d86ce9d7df5a39a56b527
Brunosrd <brunosr099@outlook.com>
2024-08-09 10:59:33 -0300

    base url added

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const { defineConfig } = require("cypress")
+
+module.exports = defineConfig ({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
   },
-};
+});
--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,7 +11,7 @@ describe('Orange HRM Test', () => {
   }
 
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
@@ -19,7 +19,7 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.dashboardGrid).should('be.visible')
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
